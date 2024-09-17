7d5e727844341a7944b1e71b84a262839711b2d7
Jonanthan Sotelo <jsotelor@udistrital.edu.co>
2024-09-17 11:26:34 -0500

    reglas parte 1

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def identificador():
         value += 1
 
 gen = identificador()
+nodos_clase = identificador()
 dot = Digraph(comment='Árbol')
 
 def TreeKD(data, column,nodoAct):
@@ -24,7 +25,10 @@ def TreeKD(data, column,nodoAct):
     order = data.sort_values(by=name)
     mitad = int(order.shape[0]/2)
     # Añadimos el valor del nodo junto a la columna a la que pertenece
-    nodoAct.value = {name : order[name].iloc[mitad], 'id': next(gen)}
+
+    # valor medio entre las dos separaciones con 8 cifras significativas
+    valor = round((order[name].iloc[mitad] + order[name].iloc[mitad-1])/2, 8)
+    nodoAct.value = {name : valor, 'id': next(gen)}
 
     # Creamos los nodos hijos
     nodoAct.hijoD = Nodo(None,None,None)
@@ -42,7 +46,6 @@ def TreeKD(data, column,nodoAct):
         elif i == 1:
             TreeKD(divisiones[i],column+1,nodoAct.hijoD)
            
-    pass
 
 def validarInstancia(df, nodo_Act) -> bool:
     # validamos si en ese grupo hay una única instancia y a que clase pertenece
@@ -73,6 +76,7 @@ def graficar(node,parentNode,isGraficable):
     llave = list(node.value.keys())
     if llave[0] == 'class':
         dot.node(str(node.value['id']),f'{llave[0]} = {node.value[llave[0]]}')
+        next(nodos_clase)
     else:
         dot.node(str(node.value['id']),f'{llave[0]} < {node.value[llave[0]]}')
 
@@ -93,6 +97,35 @@ def graficar(node,parentNode,isGraficable):
         dot.format = 'svg'  # Especificar el formato PNG
         dot.render('arbol_grafico', view=True)  # Guarda el archivo como 'arbol_grafico.png' y lo muestra
 
+def encontrar_caminos(raiz):
+    # Lista que almacenará todos los caminos desde la raíz hasta las hojas
+    caminos = []
+
+    # Función auxiliar para recorrer el árbol y encontrar los caminos
+    def dfs(nodo, camino_actual):
+        # Agregamos el nodo actual al camino
+        camino_actual.append(nodo.value)
+        
+        # Si el nodo no tiene hijos, es una hoja, añadimos el camino a la lista de caminos
+        if not hasattr(nodo, 'hijoD') and not hasattr(nodo, 'hijoI'):
+            caminos.append(list(camino_actual))  # Hacemos una copia del camino actual
+        else:
+            # Si tiene hijos, seguimos recorriendo los hijos
+            if nodo.hijoD is not None:
+                dfs(nodo.hijoD, camino_actual)
+                pass
+            if nodo.hijoI is not None:
+                dfs(nodo.hijoI, camino_actual)
+                pass
+        
+        # Al terminar de explorar este nodo, lo eliminamos del camino actual (backtracking)
+        camino_actual.pop()
+
+    # Iniciamos la búsqueda desde la raíz
+    dfs(raiz, [])
+    
+    return caminos
+
 def main():
     '''
     https://archive.ics.uci.edu/dataset/545/rice+cammeo+and+osmancik
@@ -101,8 +134,11 @@ def main():
     data = pd.read_csv('./datos.csv')
     #data = data.iloc[:1632]
     TreeKD(data,0,nodo)
-    graficar(nodo, None, True)
-    print(f'cantidad de nodos: {next(gen)-1}')
+    #graficar(nodo, None, True)
+    #print(f'cantidad de nodos clase: {next(nodos_clase)}')
+    reglas = encontrar_caminos(nodo)
+    reglas
+
 
 if __name__ == '__main__':
     main()
